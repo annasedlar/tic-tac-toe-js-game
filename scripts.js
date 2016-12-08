70fc07c7b6c7f00d1b176297e030eeda7edf4706
Anna Sedlar <annasedlar@gmail.com>
2016-12-08 10:54:37 -0500

    inital commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// user shold be able to click a box and an X/O shows up
+// put a onclick on the first square
+// when user clicks, call function that puts a X in the box
+// MILESTONE
+
+function markSquare(square){
+	document.getElementById().innerHTML = "X"
+}
